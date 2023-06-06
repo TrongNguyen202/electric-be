d8bd1fcc7a48fcaeb0d5e16d987df7f1c2872c65
appNextsol <88357126+appnextsol@users.noreply.github.com>
2023-06-06 14:23:07 +0700

    fix bug

--- src/main/java/usoft/cdm/electronics_market/controller/RoleAPI.java
+++ src/main/java/usoft/cdm/electronics_market/controller/RoleAPI.java
@@ -3,6 +3,7 @@ package usoft.cdm.electronics_market.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import usoft.cdm.electronics_market.service.RoleService;
@@ -16,6 +17,7 @@ import java.awt.print.Pageable;
 public class RoleAPI {
     private final RoleService roleService;
 
+    @GetMapping
     private ResponseEntity<?> getAll(Pageable pageable){
         return roleService.getAll(pageable);
     }
