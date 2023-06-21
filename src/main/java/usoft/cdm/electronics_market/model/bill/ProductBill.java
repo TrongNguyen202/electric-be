e8574ae0947df361c124a13a5a5cc88bb315da83
namen1706 <namen1706@gmail.com>
2023-06-21 17:34:24 +0700

    update

--- src/main/java/usoft/cdm/electronics_market/model/bill/ProductBill.java
+++ src/main/java/usoft/cdm/electronics_market/model/bill/ProductBill.java
@@ -14,16 +14,18 @@ public class ProductBill {
     private String name;
     private Integer quantity;
     private Double priceSell;
-    private Double priceAfterSell;
+    private Double priceAfterSale;
     private String img;
 
-    public ProductBill(Integer id, Integer billId, String code, String name, Integer quantity, Double priceSell, Double priceAfterSell) {
+    private Integer productId;
+
+    public ProductBill(Integer id, Integer billId, String code, String name, Integer quantity, Double priceSell, Double priceAfterSale) {
         this.id = id;
         this.billId = billId;
         this.code = code;
         this.name = name;
         this.quantity = quantity;
         this.priceSell = priceSell;
-        this.priceAfterSell = priceAfterSell;
+        this.priceAfterSale = priceAfterSale;
     }
 }
