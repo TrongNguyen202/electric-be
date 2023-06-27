78fec6ca102e004d4db8ed36b323748737edd049
appNextsol <88357126+appnextsol@users.noreply.github.com>
2023-06-27 11:31:33 +0700

    fix voucher

--- src/main/java/usoft/cdm/electronics_market/model/voucher/VoucherModel.java
+++ src/main/java/usoft/cdm/electronics_market/model/voucher/VoucherModel.java
@@ -1,11 +1,15 @@
 package usoft.cdm.electronics_market.model.voucher;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VoucherModel {
     private Integer id;
     private String code;
